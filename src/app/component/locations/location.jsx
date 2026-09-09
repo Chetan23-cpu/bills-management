164638c69dc296f2d83600938d8fa9037517ae73
Chetan23-cpu <tr.qa@tracesci.com>
2026-09-09 17:20:26 +0530

    changes

--- src/app/component/locations/location.jsx
+++ src/app/component/locations/location.jsx
@@ -19,7 +19,7 @@ const Locationpage = () => {
     try{
       const res = await fetch("/api/locations");
       const data = await res.json();
-      setLocations(Array.inArray(data) ? data: []);
+      setLocations(Array.isArray(data) ? data : []);
     } catch (err){
       console.error("Failed to fetch locations", err)
     } finally {
@@ -65,10 +65,20 @@ const Locationpage = () => {
                     </tr>
                 </thead>
                 <tbody>
-                  <tr className={styles.tablecontent}>
-                    <td className={styles.content}>1</td>
-                    <td className={styles.content}>Delhi</td>
-                    <td className={styles.content}>IT</td>
+                  {loading ? (
+                    <tr>
+                      <td colSpan="4">Loading...</td>
+                    </tr>
+                  ) : locations.length === 0 ? (
+                    <tr>
+                        <td colSpan="4">No locations found.</td>
+                    </tr>
+                  ) : (
+                    locations.map((loc, index) => (
+                  <tr className={styles.tablecontent} key={loc.id}>
+                    <td className={styles.content}>{index + 1}</td>
+                    <td className={styles.content}>{loc.name}</td>
+                    <td className={styles.content}>-</td>
                     <td className={styles.action}>
                        <div className={styles.view}><MdPageview /></div>
                        <div className={styles.edit}><MdEditDocument /></div>
@@ -76,6 +86,8 @@ const Locationpage = () => {
                         
                     </td>
                   </tr>
+                    ))
+                  )}
                 </tbody>
             </table>
           </div>
